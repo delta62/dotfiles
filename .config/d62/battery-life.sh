1ad0fe2db2815f8dc904cb50049fdb9306a9c512
Sam Noedel <sam@samnoedel.com>
2014-11-14 13:11:32 -0800

    Adding cord indicator to battery meter

--- .config/d62/battery-life.sh
+++ .config/d62/battery-life.sh
@@ -1,20 +1,67 @@
 #!/bin/bash
 
+# Bash Colors
 # COLOR_GREEN='\033[0;32m'
 # COLOR_YELLOW='\033[1;33m'
 # COLOR_RED='\033[0;31m'
 # COLOR_GRAY_DK='\033[1;30m'
 
+# Screen colors
 COLOR_WHITE='\005{w}'
 COLOR_GREEN='\005{g}'
 COLOR_YELLOW='\005{y}'
 COLOR_RED='\005{r}'
 COLOR_GRAY='\005{K}'
+COLOR_NONE='\005{-}'
+
+print_battery() {
+    # Print status bars
+    echo -en "$COLOR_WHITE[$COLOR_GREEN"
+    for i in `seq 1 $NUM_BARS`
+    do
+        PERCENTAGE=$(($i * $BAR_VALUE))
+        COLOR_END=0
+        if (($PERCENTAGE>$BATT_LIFE)) && (($COLOR_END<1))
+        then
+            COLOR_END=1
+            echo -en "$COLOR_GRAY"
+        fi
+        echo -n "|"
+    done
+    echo -en "$COLOR_WHITE]"
+
+    #print (dis)charging status
+    case "$BATT_STATE" in
+        "charged"|"charging")
+            echo -n '+'
+            ;;
+        "discharging")
+            echo -n '-'
+            ;;
+    esac
+    echo -en "$COLOR_NONE"
+}
+
+print_cord() {
+    # Print cordy thing
+    echo -en "$COLOR_WHITE=D$COLOR_GREEN"
+    for i in `seq 1 $NUM_BARS`
+    do
+        PERCENTAGE=$(($i * $BAR_VALUE))
+        COLOR_END=0
+        if (($PERCENTAGE>$BATT_LIFE)) && (($COLOR_END<1))
+        then
+            COLOR_END=1
+            echo -en "$COLOR_GRAY"
+        fi
+        echo -n "-"
+    done
+}
 
 # First capture is the percentage, second is the state ("charging", etc.)
 REGEX="([0-9]+)%; ([a-z]+)"
 BATT_STATUS=`pmset -g batt`
-NUM_BARS=10
+NUM_BARS=6
 
 # Execute the regex to capture the output
 [[ "$BATT_STATUS" =~ $REGEX ]]
@@ -35,29 +82,14 @@ else
     BATT_COLOR=$COLOR_RED
 fi
 
-# Print status bars
-echo -en "$COLOR_WHITE["
-for i in `seq 1 $NUM_BARS`
-do
-    PERCENTAGE=$(($i * $BAR_VALUE))
-    if (($PERCENTAGE<=$BATT_LIFE))
-    then
-        BAR_COLOR=$BATT_COLOR
-    else
-        BAR_COLOR=$COLOR_GRAY
-    fi
-    echo -en "$BAR_COLOR|"
-done
-echo -en "$COLOR_WHITE]"
-
-#print (dis)charging status
+# Output
 case "$BATT_STATE" in
     "charged"|"charging")
-        echo -n '+'
+        print_cord
         ;;
     "discharging")
-        echo -n '-'
+        print_battery
         ;;
 esac
 
-echo " ($BATT_LIFE%)"
+echo -en " $COLOR_WHITE($BATT_LIFE%)"
