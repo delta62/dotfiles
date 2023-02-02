7322b7a01276f42daacc1cd0ccfebcb2936314fd
Sam Noedel <sam.noedel@gmail.com>
2023-02-01 18:59:03 -0800

    Clean up vim autocommands

--- .config/nvim/lua/auto.lua
+++ .config/nvim/lua/auto.lua
@@ -1,46 +1,44 @@
 local api = vim.api
 
+local function augrp(name, cmds)
+    api.nvim_create_augroup(name, {})
+    local pattern = cmds.pattern
+
+    for _, value in ipairs(cmds) do
+        api.nvim_create_autocmd(value[1], {
+            group = name,
+            pattern = value.pattern or pattern,
+            command = value.command,
+        })
+    end
+end
+
 -- All groups: Delete trailing whitespace prior to saving
 api.nvim_create_autocmd("BufWrite", {
     pattern = "*",
     command = ":%s/\\s\\+$//e"
 })
 
-api.nvim_create_augroup('coc', {})
-api.nvim_create_autocmd('CursorHold', {
-    group = 'coc',
-    command = "silent call CocActionAsync('highlight')"
-})
-
-api.nvim_create_augroup('asm', {})
-api.nvim_create_autocmd('FileType', {
-    group = 'asm',
+augrp('asm', {
     pattern = 'asm',
-    command = 'setlocal syntax=nasm',
+    { 'FileType', command = 'setlocal syntax=nasm' },
 })
 
-api.nvim_create_augroup('quickfix', {})
-api.nvim_create_autocmd('FileType', {
-    group = 'quickfix',
-    pattern = 'qf',
-    command = 'setlocal wrap',
+augrp('coc', {
+    { 'CursorHold', command = "silent call CocActionAsync('highlight')" },
 })
 
-api.nvim_create_augroup('rust', {})
-api.nvim_create_autocmd('FileType', {
-    group = 'rust',
-    pattern = "rust",
-    command = "setlocal formatprg=cargo\\ fmt",
+augrp('rust', {
+    pattern = 'rust',
+    { 'FileType', command = "setlocal formatprg=cargo\\ fmt" },
 })
 
-api.nvim_create_augroup('typescript', {})
-api.nvim_create_autocmd('FileType', {
-    group = 'typescript',
+augrp('quickfix', {
+    { 'FileType',  pattern = 'qf', command = 'setlocal wrap' },
+})
+
+augrp('typescript', {
     pattern = 'typescript*',
-    command = 'setlocal cc=81 tw=80 sts=2 sw=2 fex&',
-})
-api.nvim_create_autocmd('FileType', {
-    group = 'typescript',
-    pattern = 'typescript*',
-    command = 'setlocal formatprg=prettier\\ --parser\\ typescript',
+    { 'FileType', command = 'setlocal cc=81 tw=80 sts=2 sw=2 fex&' },
+    { 'FileType', command = 'setlocal formatprg=prettier\\ --parser\\ typescript' },
 })
